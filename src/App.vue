c0d73c15d2e84ca6b3018f6dec74f06098ba5831
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-05-15 00:01:51 -0500

    feat(app-vue): add component router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script>
-import SideNavbar from './public/components/side-navbar.component.vue'
+import SideNavbar from './public/components/side-navbar.component.vue'; // Assuming this is your navbar
+
 export default {
-    components: {SideNavbar},
+    name: 'App',
+    components: { SideNavbar },
     data(){
       return {
-        items:[]
+        items:[
+          {label: 'option.status', to:'/status'},
+          {label: 'option.record', to:'/record'},
+        ]
       }
     }
 }
 </script>
 
 <template>
-  <side-navbar/>
+  <div style="display: flex;">
+    <side-navbar :items="items" />
+    <div style="flex-grow: 1; padding: 1rem;">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <style scoped>

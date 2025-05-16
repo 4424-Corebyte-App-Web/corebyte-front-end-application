6bc9d128932c71396b806da8c8b0398cbc7c1d46
RenzoVi21 <renzovillata@gmail.com>
2025-05-16 00:10:56 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,38 @@
+<script>
+import SideNavbar from './public/components/side-navbar.component.vue';
 
+export default {
+  name: 'App',
+  components: { SideNavbar },
+  data() {
+    return {
+      isLoggedIn: false,
+      items: [
+        { label: 'option.status', to: '/status' },
+        { label: 'option.record', to: '/record' },
+      ]
+    }
+  },
+  methods: {
+    onLogin() {
+      this.isLoggedIn = true;
+    }
+  }
+}
+</script>
+
+<template>
+  <div style="display: flex;">
+    <!-- Mostrar sidebar solo si el usuario está logueado -->
+    <side-navbar v-if="isLoggedIn" :items="items" />
+    <div style="flex-grow: 1; padding: 1rem;">
+      <!-- Reenviamos eventos del componente cargado en router-view -->
+      <router-view v-slot="{ Component }">
+        <component :is="Component" @login="onLogin" />
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>

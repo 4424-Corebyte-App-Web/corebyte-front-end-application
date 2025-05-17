795dd03f0e5b37bc3629c6f2f3f97e967eedf0f4
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-05-17 10:16:24 -0500

    chore batchService.js

---
+++ src/batch-management/services/batchService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/batches';
+
+export const getAllBatches = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('Error al obtener todos los lotes:', error);
+    throw error;
+  }
+};
+
+export const getBatchById = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`);
+  return response.data;
+};
+
+export const updateBatch = async (id, batchData) => {
+  try {
+    const response = await axios.put(`${API_URL}/${id}`, batchData);
+    return response.data;
+  } catch (error) {
+    console.error('Error al actualizar el lote:', error);
+    throw error;
+  }
+};

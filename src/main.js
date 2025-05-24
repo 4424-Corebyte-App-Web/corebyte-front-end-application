99044cb18b35d9945c3b9f5f86ab1d991b815210
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-05-24 13:22:13 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,21 +3,28 @@ import './style.css'
 import App from './App.vue'
 import i18n from "./i18n";
 import PrimeVue from 'primevue/config';
-import Menu from 'primevue/menu';
-import Avatar from 'primevue/avatar';
-import SelectButton from 'primevue/selectbutton';
-import 'primeicons/primeicons.css';
-import AutoComplete from 'primevue/autocomplete';
-import ToastService from 'primevue/toastservice';
-import Button from 'primevue/button';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
 import router from './routes/index.js';
-import Dialog from 'primevue/dialog';
-import Checkbox from 'primevue/checkbox';
-import CheckboxGroup from 'primevue/checkboxgroup';
 import {jsPDF} from 'jspdf';
 import autoTable from 'jspdf-autotable';
+import {
+    Menu,
+    Avatar,
+    SelectButton, 
+    DatePicker,
+    Textarea,
+    AutoComplete,
+    ToastService,
+    Button,
+    DataTable,
+    Column,
+    Dialog,
+    Checkbox,
+    CheckboxGroup,
+    InputNumber,
+    InputText,
+    Dropdown 
+} from "primevue";
+import 'primeicons/primeicons.css';
 
 
 const app = createApp(App); 
@@ -30,6 +37,11 @@ app.component("pv-avatar", Avatar);
 app.component("pv-selectbutton", SelectButton);
 app.component("pv-autocomplete", AutoComplete);
 app.component("pv-button", Button);
+app.component("pv-inputnumber", InputNumber);
+app.component("pv-inputtext", InputText);
+app.component("pv-dropdown", Dropdown);
+app.component("pv-textarea", Textarea);
+app.component("pv-datepicker", DatePicker);
 app.component("pv-datatable", DataTable);
 app.component("pv-column", Column);
 app.component("pv-dialog", Dialog);

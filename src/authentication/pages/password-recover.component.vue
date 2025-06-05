cc9f10fe369b987217ff4414d6979cb2cb4c857d
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-06-04 22:37:40 -0500

    Update password-recover.component.vue

--- src/authentication/pages/password-recover.component.vue
+++ src/authentication/pages/password-recover.component.vue
@@ -1,23 +1,31 @@
+<script>
+export default {
+  name: "PasswordRecover",
+};
+</script>
+
 <template>
-    <div class="container">
-      <h2>{{ $t("password-recover.title") }}</h2>
-      <p>{{ $t("password-recover.text") }}</p>
-      <router-link to="/login" class="btn">{{ $t("password-recover.button") }}</router-link>
-    </div>
-  </template>
-  
-  <style scoped>
-  .container {
-    text-align: center;
-    margin-top: 100px;
-    color: white;
-  }
-  .btn {
-    color: #ffcc33;
-    border: 1px solid #ffcc33;
-    padding: 0.5rem 1.5rem;
-    border-radius: 25px;
-    display: inline-block;
-    margin-top: 20px;
-  }
-  </style>
+  <div class="container">
+    <h2>{{ $t("password-recover.title") }}</h2>
+    <p>{{ $t("password-recover.text") }}</p>
+    <router-link to="/login" class="btn">{{
+      $t("password-recover.button")
+    }}</router-link>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 100px;
+  color: white;
+}
+.btn {
+  color: #ffcc33;
+  border: 1px solid #ffcc33;
+  padding: 0.5rem 1.5rem;
+  border-radius: 25px;
+  display: inline-block;
+  margin-top: 20px;
+}
+</style>

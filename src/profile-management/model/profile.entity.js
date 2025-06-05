623e76985fed775d747885c74fd004aa33765f62
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-06-04 22:49:21 -0500

    Update profile.entity.js

--- src/profile-management/model/profile.entity.js
+++ src/profile-management/model/profile.entity.js
@@ -1,16 +1,15 @@
 export class Profile {
-    constructor(
-        {
-            id = '',
-            name = '',
-            email = '',
-            phone = '',
-            avatar = '',
-            accountType = 'basic',
-        } = {}) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.role =role;
-    }
+  constructor({
+    id = "",
+    name = "",
+    email = "",
+    phone = "",
+    avatar = "",
+    accountType = "basic",
+  } = {}) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.role = role;
+  }
 }

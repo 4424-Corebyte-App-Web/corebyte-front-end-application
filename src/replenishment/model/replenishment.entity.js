24e5021738d83e5e532fbf4aa4fd196d87904dca
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-06-04 22:50:23 -0500

    Update replenishment.entity.js

--- src/replenishment/model/replenishment.entity.js
+++ src/replenishment/model/replenishment.entity.js
@@ -1,20 +1,12 @@
 export class Replenishment {
-    constructor({
-        id,
-        name,
-        type,
-        date,
-        stockActual,
-        stockMinimo,
-        price}
-
-    ) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.date = date;
-        this.stockActual = stockActual;
-        this.stockMinimo = stockMinimo;
-        this.price = price;
-    }
+  constructor({ id, name, type, date, stockActual, stockMinimo, price }) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.date = date;
+    this.stockActual = stockActual;
+    this.stockMinimo = stockMinimo;
+    this.price = price;
+    S;
+  }
 }
